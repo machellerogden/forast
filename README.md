fb7bc72584dc547ffc5f0b733a00db2da74c4921
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-17 09:52:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forast
+
+> ESTree-compliant AST Factories
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const nodes = require('require-dir')('./lib/nodes');
+const trees = require('require-dir')('./lib/trees');
+const cast = require('./lib/cast');
+
+module.exports = {
+    nodes,
+    trees,
+    cast
+};
